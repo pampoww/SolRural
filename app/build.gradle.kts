947e17101731062e1219b5065840d6a9084f9b7e
pamschimalesky <rm551600@fiap.com.br>
2024-11-13 18:39:30 -0300

    implementação do picasso e adição de activitys

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "br.com.dominiodaaplicao.solrural"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.com.dominiodaaplicao.solrural"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.cardview)
+    implementation (libs.picasso)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/br/com/dominiodaaplicao/solrural/activity/MainActivity.kt
+++ app/src/main/java/br/com/dominiodaaplicao/solrural/activity/MainActivity.kt
@@ -1,21 +1,111 @@
 package br.com.dominiodaaplicao.solrural.activity
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import br.com.dominiodaaplicao.solrural.R
+import androidx.cardview.widget.CardView
+import com.squareup.picasso.Picasso
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
+        window.decorView.setBackgroundColor(Color.WHITE)
+
+        val logoImageView: ImageView = findViewById(R.id.logoImageView)
+        val imgLerSobre: ImageView = findViewById(R.id.imgLerSobre)
+        val imgVer: ImageView = findViewById(R.id.imgVer)
+        val imgCadastro : ImageView = findViewById(R.id.imgCadastro)
+        val setaVer: ImageView = findViewById(R.id.setaVer)
+        val setaCadastro: ImageView = findViewById((R.id.setaCadastro))
+        val setaLerSobre: ImageView = findViewById(R.id.setaLerSobre)
+
+        Picasso.get()
+            .load(R.drawable.sol_rural)
+            .resize(400, 170)
+            .centerInside()
+            .into(logoImageView)
+
+        Picasso.get()
+            .load(R.drawable.agrovoltaico)
+            .resize(80, 80)
+            .centerInside()
+            .into(imgLerSobre)
+
+        Picasso.get()
+            .load(R.drawable.fazendas_cadastradas)
+            .resize(80, 80)
+            .centerInside()
+            .into(imgVer)
+
+        Picasso.get()
+            .load(R.drawable.cadastro)
+            .resize(80, 80)
+            .centerInside()
+            .into(imgCadastro)
+
+        Picasso.get()
+            .load(R.drawable.seta_direita)
+            .resize(46, 35)
+            .centerInside()
+            .into(setaVer)
+
+        Picasso.get()
+            .load(R.drawable.seta_direita)
+            .resize(46, 35)
+            .centerInside()
+            .into(setaCadastro)
+
+        Picasso.get()
+            .load(R.drawable.seta_direita)
+            .resize(46, 35)
+            .centerInside()
+            .into(setaLerSobre)
+
+        val cardLerSobre: CardView = findViewById(R.id.cardLerSobre)
+        val cardParteDoProjeto: CardView = findViewById(R.id.cardParteDoProjeto)
+        val cardCadastro: CardView = findViewById(R.id.cardCadastro)
+        val botaoLogin: Button = findViewById(R.id.btn_login)
+
+        cardLerSobre.setOnClickListener {
+
+            val intent = Intent(this, LerSobreActivity::class.java)
+            startActivity(intent)
+        }
+
+        cardParteDoProjeto.setOnClickListener {
+
+            val intent = Intent(this, ParteDoProjetoActivity::class.java)
+            startActivity(intent)
+        }
+
+        cardCadastro.setOnClickListener {
+
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
+
+        botaoLogin.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
