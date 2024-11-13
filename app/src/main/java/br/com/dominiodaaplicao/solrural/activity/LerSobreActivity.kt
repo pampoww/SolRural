58c5c5d7375bb55f83f73639d6bb729ed2dc2e25
pamschimalesky <rm551600@fiap.com.br>
2024-11-13 20:45:10 -0300

    Criando layout e navegação da página "sobre o projeto"

--- app/src/main/java/br/com/dominiodaaplicao/solrural/activity/LerSobreActivity.kt
+++ app/src/main/java/br/com/dominiodaaplicao/solrural/activity/LerSobreActivity.kt
@@ -1,4 +1,35 @@
 package br.com.dominiodaaplicao.solrural.activity
 
-class LerSobreActivity {
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import br.com.dominiodaaplicao.solrural.R
+
+class LerSobreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ler_sobre)
+
+
+        val imageView: ImageView = findViewById(R.id.contentImage)
+        val backButton: ImageButton = findViewById(R.id.backButton)
+        val textView: TextView = findViewById(R.id.contentText)
+
+
+        backButton.setOnClickListener{
+            finish()
+        }
+
+        Picasso.get()
+            .load(R.drawable.exemplo_agrovoltaico)
+            .resize(200, 200)
+            .centerInside()
+            .into(imageView)
+
+        textView.text = getString(R.string.ler_sobre_texto)
+    }
 }
