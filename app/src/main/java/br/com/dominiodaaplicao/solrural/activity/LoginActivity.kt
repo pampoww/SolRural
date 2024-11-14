7c6c196146d5236473e3c48ebf3ef7aeece1cc46
pamschimalesky <rm551600@fiap.com.br>
2024-11-14 19:03:29 -0300

    ajustes nos botoes de voltar

--- app/src/main/java/br/com/dominiodaaplicao/solrural/activity/LoginActivity.kt
+++ app/src/main/java/br/com/dominiodaaplicao/solrural/activity/LoginActivity.kt
@@ -1,4 +1,74 @@
 package br.com.dominiodaaplicao.solrural.activity
 
-class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import br.com.dominiodaaplicao.solrural.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var registerButton: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        emailEditText = findViewById(R.id.email)
+        passwordEditText = findViewById(R.id.password)
+        registerButton = findViewById(R.id.registerButton)
+
+        val backButton: ImageButton = findViewById(R.id.backButton)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        registerButton.setOnClickListener {
+            signInWithEmailAndPassword()
+        }
+    }
+
+    private fun signInWithEmailAndPassword() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task: Task<AuthResult> ->
+                if (task.isSuccessful) {
+                    Log.d("Firebase", "signInWithEmail:success")
+                    Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+                    // Navigate to the next activity (e.g., your main activity)
+
+                } else {
+                    Log.w("Firebase", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
